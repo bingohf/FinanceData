0106bcfbf00114f4b7790dc9475cbdf922359f85
bingohf <togb@qq.com>
2016-05-01 22:59:06 +0800

    download all page data

--- src/main/java/com/togb/finance/margin_trading/DownloadHTML.java
+++ src/main/java/com/togb/finance/margin_trading/DownloadHTML.java
@@ -3,9 +3,13 @@ package com.togb.finance.margin_trading;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
+
 import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import rx.Observable;
 import rx.Observable.OnSubscribe;
@@ -25,7 +29,7 @@ public class DownloadHTML {
 						// TODO Auto-generated method stub
 						return Observable.create(new OnSubscribe<File>(){
 
-							public void call(Subscriber<? super File> subscriber) {
+							public void call(final Subscriber<? super File> subscriber) {
 								// TODO Auto-generated method stub
 								File file = new File(TEMP_FLODER + stockNo +"/");
 								if (!file.exists()){
@@ -44,7 +48,38 @@ public class DownloadHTML {
 									}
 
 									public void onNext(File file) {
-										
+										try {
+											Document document = Jsoup.parse(file, "GB2312");
+											String pageInfo = document.select(".page_info").text();
+											int pageSize = Integer.parseInt(pageInfo.split("\\/")[1]);
+											Observable.range(2, pageSize).flatMap(new Func1<Integer, Observable<? extends File>>() {
+
+												public Observable<? extends File> call(Integer t) {
+													// TODO Auto-generated method stub
+													return downloadDetail(stockNo, t);
+												}
+											}).subscribe(new Subscriber() {
+
+												public void onCompleted() {
+													// TODO Auto-generated method stub
+													
+												}
+
+												public void onError(Throwable e) {
+													// TODO Auto-generated method stub
+													
+												}
+
+												public void onNext(Object t) {
+													// TODO Auto-generated method stub
+													
+												}
+											});
+										} catch (IOException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+											subscriber.onError(e);
+										}
 										
 									}
 								});
@@ -61,7 +96,9 @@ public class DownloadHTML {
 			public void call(Subscriber<? super File> subscriber) {
 				try {
 					File pageFile = new File(TEMP_FLODER + stockNo +"/" + pageIndex +".html");
-					URL website = new URL(String.format(URL, stockNo,pageIndex));
+					String url = String.format(URL, stockNo,pageIndex);
+					URL website = new URL(url);
+					System.out.println(url);
 					FileUtils.copyURLToFile(website, pageFile);
 					subscriber.onNext(pageFile);
 					subscriber.onCompleted();
